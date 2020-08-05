e4d735ee5f4c24073458bbf289f33d7e8d0c60ec
r-heap <52332957+r-heap@users.noreply.github.com>
2020-08-05 12:27:11 -0700

    Update form.js

--- form.js
+++ form.js
@@ -55,4 +55,5 @@ form.addEventListener('submit', (event) => {
         valid = validateEmail(email)
         var submit_email= document.getElementById('email_submission').value;
         heap.addUserProperties({'email': submit_email});
+        heap.identify(submit_email);
     }});
